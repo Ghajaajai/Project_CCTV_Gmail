b9a286a5c2848fbd4a98da7ffab86985f3f98991
152021166 Muhammad Ghaza Azhar Lesmana <114157498+Ghajaajai@users.noreply.github.com>
2024-06-07 23:37:50 +0700

    Project_CCTV_Gmail
    go

---
+++ Esp32 Gmail/Projec_CCTV_Gmail/Projec_CCTV_Gmail.ino
@@ -0,0 +1,231 @@
+
+#include "esp_camera.h"
+#include "SPI.h"
+#include "driver/rtc_io.h"
+#include "ESP32_MailClient.h"
+#include <FS.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
+#define emailSenderAccount    "ghajaajay@gmail.com"
+#define emailSenderPassword   "pinginsiaran666"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465
+#define emailSubject          "ESP32-CAM Photo Captured"
+#define emailRecipient        "muhammad.ghaza@mhs.itenas.ac.id"
+
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+ 
+#define FILE_PHOTO "/photo.jpg" //--> Photo File Name to save in SPIFFS
+
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "Gatau";
+const char* password = "Kontrakan4";
+
+SMTPData smtpData; //--> The Email Sending data object contains config and data to send
+
+void capturePhotoSaveSpiffs( void ) {
+  camera_fb_t * fb = NULL; //--> pointer
+  bool ok = 0; //--> Boolean indicating if the picture has been taken correctly
+
+  do {
+    Serial.println("Taking a photo...");
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed.");
+      return;
+    }
+
+    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+    if (!file) {
+      Serial.println("Failed to open file in writing mode.");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.print("The picture has been saved in ");
+      Serial.print(FILE_PHOTO);
+      Serial.print(" - Size: ");
+      Serial.print(file.size());
+      Serial.println(" bytes.");
+    }
+    
+    file.close(); //--> Close the file
+    esp_camera_fb_return(fb); //--> return the frame buffer back to the driver for reuse.
+    Serial.println("Checking if the picture file has been saved correctly in SPIFFS...");
+    ok = checkPhoto(SPIFFS);
+    if (ok == 1) {
+      Serial.println("The picture file has been saved correctly in SPIFFS.");
+    } else {
+      Serial.println("The picture file is not saved correctly in SPIFFS.");
+      Serial.println("Recapture photos.");
+      Serial.println();
+    }
+  } while ( !ok );
+ 
+}
+
+bool checkPhoto( fs::FS &fs ) {
+  File f_pic = fs.open( FILE_PHOTO );
+  unsigned int pic_sz = f_pic.size();
+  return ( pic_sz > 100 );
+}
+
+void sendPhoto( void ) {
+  Serial.println("Sending email...");
+
+  // Set the SMTP Server Email host, port, account and password
+  smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword); 
+  
+  // Set the sender name and Email
+  smtpData.setSender("ESP32-CAM UTEH STR", emailSenderAccount); 
+  
+  // Set Email priority or importance High, Normal, Low or 1 to 5 (1 is highest)
+  smtpData.setPriority("High"); 
+
+  // Set the subject
+  smtpData.setSubject(emailSubject); 
+
+  // Set the email message in HTML format
+  smtpData.setMessage("<h2>Photo captured with ESP32-CAM and attached in this email.</h2>", true); 
+  // Set the email message in text format
+  //smtpData.setMessage("Photo captured with ESP32-CAM and attached in this email.", false); 
+
+  // Add recipients, can add more than one recipient
+  smtpData.addRecipient(emailRecipient); 
+  //smtpData.addRecipient(emailRecipient2);
+
+  // Add attach files from SPIFFS
+  smtpData.addAttachFile(FILE_PHOTO, "image/jpg"); 
+
+  // Set the storage type to attach files in your email (SPIFFS)
+  smtpData.setFileStorageType(MailClientStorageType::SPIFFS); 
+
+  // sendCallback
+  smtpData.setSendCallback(sendCallback); 
+  
+  // Start sending Email, can be set callback function to track the status
+  if (!MailClient.sendMail(smtpData))
+  Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
+
+  // Clear all data from Email object to free memory
+  smtpData.empty();
+}
+
+// Callback function to get the Email sending status
+void sendCallback(SendStatus msg) {
+  Serial.println(msg.info()); //--> Print the current status
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //--> disable brownout detector
+  
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println();
+
+ 
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Successfully connected to ");
+  Serial.println(ssid);
+
+  Serial.print("IP Address: http://");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.println("Starting to mount SPIFFS...");
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    Serial.println("ESP32 Cam Restart...");
+    ESP.restart();
+  }
+  else {
+    delay(500);
+    Serial.println("SPIFFS mounted successfully");
+  }
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA; //--> FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 11 ;
+    config.fb_count = 1;
+  }
+
+  Serial.println();
+  Serial.println("Camera initialization...");
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  Serial.print("Camera initialization was successful.");
+  Serial.println();
+ 
+  
+  capturePhotoSaveSpiffs(); //--> Calling the capturePhotoSaveSpiffs() subroutine.
+  sendPhoto(); //--> Calling the sendPhoto() subroutine.
+  
+  Serial.println();
+  Serial.println("Press the Reset button to :");
+  Serial.println("- Reconnecting WiFi");
+  Serial.println("- Take and send new photos.");
+  Serial.println();
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1);
+}
